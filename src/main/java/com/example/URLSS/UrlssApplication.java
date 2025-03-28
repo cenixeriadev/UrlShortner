beaa902109a859ea3f7c532930d752d0fdac173c
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-03-28 14:39:18 -0500

    Project Setup

---
+++ src/main/java/com/example/URLSS/UrlssApplication.java
@@ -0,0 +1,13 @@
+package com.example.URLSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/URLSS/UrlssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.URLSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
