219527e728d29e042c7b726108ead49897e7bdeb
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-09-02 18:41:02 -0500

    update sql generate db

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE short_urls (
     id SERIAL  PRIMARY KEY,
     url TEXT NOT NULL,
-    short_code VARCHAR(10) UNIQUE NOT NULL,
+    short_code VARCHAR(20) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     access_count INT DEFAULT 0
